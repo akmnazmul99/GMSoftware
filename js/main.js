2e0a490c593fbc8e46677145241ad2e6c82979ef
A.K.M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2016-09-03 00:57:13 +0600

    final update

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// slick nav for mobile menu
+$(document).ready(function() {
+    $('#menu').slicknav();
+});
+
+
+// client slider
+$(window).load(function() {
+    $("#client_slider").flexisel({
+        visibleItems: 5,
+        itemsToScroll: 1,
+        autoPlay: {
+            enable: true,
+            interval: 3000,
+            pauseOnHover: true
+        }
+    });
+});
+
+
